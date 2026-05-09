de41c06c314a8d30b77a2fc48c35c9b052419925
JaimeArturoMaganaNerio <neriojaime9@gmail.com>
2026-05-08 21:38:25 -0600

    FEAT: Agregando configuracion de DB y variables de entorno

---
+++ Hogwart/src/test/java/com/example/hogwart/HogwartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hogwart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
